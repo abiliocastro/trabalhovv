0389ddf96edba7574d193dbd7b2f4dba33c6d981
Leandro Beserra do Nascimento <leandrobeserra11@gmail.com>
2019-04-01 19:37:23 -0300

    Iniciando Crud de produto

---
+++ src/entidade/Produto.java
@@ -0,0 +1,58 @@
+package entidade;
+
+public class Produto {
+
+	private long id;
+	private String nome; 
+	private float preco;
+	private int quantidade;
+	private String lojaFornecedora;
+	
+	public Produto(long id, String nome, float preco, int quantidade, String lojaFornecedora) {
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.lojaFornecedora = lojaFornecedora;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getLojaFornecedora() {
+		return lojaFornecedora;
+	}
+
+	public void setLojaFornecedora(String lojaFornecedora) {
+		this.lojaFornecedora = lojaFornecedora;
+	}
+}
---
+++ src/testes/ControladorProdutoTeste.java
@@ -0,0 +1,14 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControladorProdutoTeste {
+
+	@Test
+	void cadastrarProduto() {
+	
+	}
+
+}
