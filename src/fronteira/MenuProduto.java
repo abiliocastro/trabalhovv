48ca20b0c4297e777d77310bb9588cfb49e85b2d
Leandro Beserra do Nascimento <leandrobeserra11@gmail.com>
2019-04-02 12:02:22 -0300

    Atualizando classe Menu Produto

--- src/fronteira/MenuProduto.java
+++ src/fronteira/MenuProduto.java
@@ -10,7 +10,7 @@ public class MenuProduto {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1- Listar | 2- Cadastrar | 3- Editar | 4- Excluir | 0 - Sair\nDigite a opc:");
-		int entrada = sc.nextInt();
+		int entrada = Integer.parseInt(sc.nextLine());
 		
 		switch (entrada) {
 		case 0:
@@ -18,17 +18,25 @@ public class MenuProduto {
 			break;
 		case 1:
 			break;
-		case 2:			
-			System.out.println("Digite o nome do produto: ");
-			String nome = sc.next();
-			System.out.println("Digite o preço do produto: ");
-			float preco = sc.nextFloat();
-			System.out.println("Digite a quantidade do produto: ");
-			int quantidade = sc.nextInt();
-			System.out.println("Digite a loja fornecedora do produto: ");
-			String lojaFornecedora= sc.next();
-			
-			cp.cadastrarProduto(nome, preco, quantidade, lojaFornecedora);
+		case 2:		
+			try {
+				System.out.println("Digite o nome do produto: ");
+				String nome = sc.nextLine();
+				System.out.println("Digite o preço do produto: ");
+				float preco = Float.parseFloat(sc.nextLine());
+				System.out.println("Digite a quantidade do produto: ");
+				int quantidade = Integer.parseInt(sc.nextLine());
+				System.out.println("Digite a loja fornecedora do produto: ");
+				String lojaFornecedora= sc.nextLine();
+				
+				cp.cadastrarProduto(nome, preco, quantidade, lojaFornecedora);
+			}catch (NumberFormatException e) {
+				System.out.println("formato da entrada invalida");
+			}catch (Exception e) {
+				
+			}finally {
+				
+			}
 			break;
 		case 3:
 			break;
