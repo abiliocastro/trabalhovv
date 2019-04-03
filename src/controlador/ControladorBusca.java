41dc5fd2b72ff0ac02257b123ad726536229f390
Abilio Castro <castro.abilio@gmail.com>
2019-04-03 11:20:39 -0300

    Contralador de Busca OK

--- src/controlador/ControladorBusca.java
+++ src/controlador/ControladorBusca.java
@@ -1,34 +1,27 @@
 package controlador;
 
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import entidade.Produto;
 import fronteira.RepositorioProduto;
 
 public class ControladorBusca {
-		static RepositorioProduto repositorioDeProduto;
+		RepositorioProduto repositorioDeProduto;
 		ArrayList<Produto> produtosBuscados;
-		private static Pattern buscarP;
+		ArrayList<Produto> todosProdutos;
 		
 		public ControladorBusca() {
 			repositorioDeProduto = RepositorioProduto.getInstance();
-			produtosBuscados = new ArrayList<Produto>();
+			//todosProdutos = ;
 		} 
 	
-		public ArrayList<Produto> buscarProduto(String produtoBuscado) {
-			try {
-				buscarP = Pattern.compile(produtoBuscado);
-				if(repositorioDeProduto.getProdutos().size()>0) {
-					for (Produto produto : repositorioDeProduto.getProdutos()) {
-						Matcher m = buscarP.matcher(produto.getNome());
-						if(m.find()) {
-							produtosBuscados.add(produto);
-						}
-					}
-				}	
-			}catch (Exception e) {}
+		public ArrayList<Produto> buscarProduto(String termoBuscado) {
+			produtosBuscados = new ArrayList<Produto>();
+			for (Produto produto : repositorioDeProduto.getProdutos()) {
+				if(produto.match(termoBuscado)) {
+					produtosBuscados.add(produto);
+				}
+			}
 			return produtosBuscados;
 		}
 }
--- src/testes/ControladorBuscaTeste.java
+++ src/testes/ControladorBuscaTeste.java
@@ -9,37 +9,30 @@ import org.junit.jupiter.api.Test;
 import controlador.ControladorBusca;
 import entidade.Produto;
 import fronteira.Main;
-import fronteira.RepositorioProduto;
 
 class ControladorBuscaTeste {
-	ArrayList<Produto> produtosBuscado;
-	static RepositorioProduto repositorioDeProduto = RepositorioProduto.getInstance();
 	ControladorBusca contBusc = new ControladorBusca();
+	ArrayList<Produto> produtosBuscado;
 	
 	@Test
 	void buscarProduto() {
 		Main.inicializarSistema();
-		produtosBuscado = contBusc.buscarProduto("Iphone");
+		String termoBuscar = "ruim";
+		produtosBuscado = contBusc.buscarProduto(termoBuscar);
+		System.out.println(produtosBuscado.size());
 		if(!produtosBuscado.isEmpty()) {
 			for (Produto produto : produtosBuscado) {
-				System.out.println(produto.getNome());
-				assertTrue(produto.getNome().equals("xxx"));
+				assertTrue(produto.getNome().toLowerCase().contains(termoBuscar.toLowerCase()));
 			}
 		} else {
-			System.out.println("Array Vazio");
+			assertEquals(0, produtosBuscado.size());
 		}
-		
 	}
 
 	@Test
-	void buscarProdutoNulo() {
-		ArrayList<Produto> produtosBuscado = new ArrayList<Produto>(); 
-		Produto P = null;				
-		repositorioDeProduto.getProdutos().add(P);
+	void buscarProdutoNulo() { 			
 		String atual = null;
 		produtosBuscado = contBusc.buscarProduto(atual);
-		for (Produto produto : produtosBuscado) {
-			assertNotNull(produto);
-		}
+		assertTrue(produtosBuscado.isEmpty());
 	}
 }
