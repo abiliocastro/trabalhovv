b49eb51987464c9417894bcf0ff615a9a2f2277b
Abilio Castro <castro.abilio@gmail.com>
2019-04-04 14:18:52 -0300

    Preparando para merge

--- src/testes/InterfaceProdutoTeste.java
+++ src/testes/InterfaceProdutoTeste.java
@@ -34,34 +34,34 @@ public class InterfaceProdutoTeste{
 	}
 
 	// Testando saida do caso cadastrar
-//	@Test
-//	void menuProdutoCadastrar() throws Exception {
-//		Main.inicializarSistema();
-//		this.mudarSaida();
-//		mp.selecionarOpcao(2);
-//		String mostrado = this.capturarSaida();
-//		String esperado;
-//		if(os.equals("linux"))
-//			esperado = "Digite o nome do produto: \nDigite o preco do produto: \nDigite a quantidade do produto: \nDigite a loja fornecedora do produto: \nproduto cadastrado com sucesso\n";
-//		else
-//			esperado = "Digite o nome do produto: \r\nDigite o preco do produto: \r\nDigite a quantidade do produto: \r\nDigite a loja fornecedora do produto: \r\nproduto cadastrado com sucesso\r\n";
-//		assertEquals(esperado, mostrado);
-//	}
+	@Test
+	void menuProdutoCadastrar() throws Exception {
+		Main.inicializarSistema();
+		this.mudarSaida();
+		mp.selecionarOpcao(2);
+		String mostrado = this.capturarSaida();
+		String esperado;
+		if(os.equals("linux"))
+			esperado = "Digite o nome do produto: \nDigite o preco do produto: \nDigite a quantidade do produto: \nDigite a loja fornecedora do produto: \nproduto cadastrado com sucesso\n";
+		else
+			esperado = "Digite o nome do produto: \r\nDigite o preco do produto: \r\nDigite a quantidade do produto: \r\nDigite a loja fornecedora do produto: \r\nproduto cadastrado com sucesso\r\n";
+		assertEquals(esperado, mostrado);
+	}
 	
-//	@Test
-//	void menuProdutoEditar() throws Exception {
-//		Main.inicializarSistema();
-//		this.mudarSaida();
-//		mp.selecionarOpcao(2);
-//		String mostrado = this.capturarSaida();
-//		System.out.println(mostrado);
-//		String esperado;
-//		if(os.equals("linux"))
-//			esperado = "Digite o nome do produto: \nDigite o preco do produto: \nDigite a quantidade do produto: \nDigite a loja fornecedora do produto: \nproduto cadastrado com sucesso\n";
-//		else
-//			esperado = "Digite o nome do produto: \r\nDigite o preco do produto: \r\nDigite a quantidade do produto: \r\nDigite a loja fornecedora do produto: \r\nproduto cadastrado com sucesso\r\n";
-//		assertEquals(esperado, mostrado);
-//	}
+	@Test
+	void menuProdutoEditar() throws Exception {
+		Main.inicializarSistema();
+		this.mudarSaida();
+		mp.selecionarOpcao(2);
+		String mostrado = this.capturarSaida();
+		System.out.println(mostrado);
+		String esperado;
+		if(os.equals("linux"))
+			esperado = "Digite o nome do produto: \nDigite o preco do produto: \nDigite a quantidade do produto: \nDigite a loja fornecedora do produto: \nproduto cadastrado com sucesso\n";
+		else
+			esperado = "Digite o nome do produto: \r\nDigite o preco do produto: \r\nDigite a quantidade do produto: \r\nDigite a loja fornecedora do produto: \r\nproduto cadastrado com sucesso\r\n";
+		assertEquals(esperado, mostrado);
+	}
 	
 	@Test
 	void menuProdutoExcluirIdExistente() {
