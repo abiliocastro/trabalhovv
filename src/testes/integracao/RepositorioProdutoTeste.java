f66ae8dfa8960b36dbbbe3b1286a1e1d35a593ff
douglasdm <douglasmdamasceno@gmail.com>
2019-05-09 17:39:36 -0300

    iniciando teste de lerProdutos

--- src/testes/integracao/RepositorioProdutoTeste.java
+++ src/testes/integracao/RepositorioProdutoTeste.java
@@ -1,13 +1,21 @@
 package testes.integracao;
 
+
+import static org.junit.Assert.assertEquals;
+
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import entidade.Produto;
 import fronteira.database.Conexao;
 import fronteira.database.RepositorioProduto;
 
@@ -21,7 +29,13 @@ public class RepositorioProdutoTeste {
 	@Mock
 	private PreparedStatement stmt = Mockito.mock(PreparedStatement.class);
 	
-	RepositorioProduto rp = RepositorioProduto.getInstance();
+	@Mock
+	private ResultSet resultSet = Mockito.mock(ResultSet.class);
+	
+	@Mock
+	private Statement statement = Mockito.mock(Statement.class);
+	
+	Produto produto = null;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -36,20 +50,37 @@ public class RepositorioProdutoTeste {
 		
 	}
 	@Before
-	public void setLer() {
-	//	Mockito.when(methodCall)
+	public void setLer() throws SQLException {
+		//Mockito.when(con.getConexao()).thenReturn(c);
+		Mockito.when(c.createStatement().executeQuery("SELECT")).thenReturn(resultSet);
+		
+		produto = new Produto(1, "TV", 1500, 2, "EletroMoveis");
+		
+		Mockito.when(resultSet.next()).thenReturn(true);
+		
+		Mockito.when(resultSet.getInt(1)).thenReturn((int) produto.getId());
+		Mockito.when(resultSet.getString(2)).thenReturn(produto.getNome());
+		Mockito.when(resultSet.getFloat(3)).thenReturn(produto.getPreco());
+		Mockito.when(resultSet.getInt(4)).thenReturn(produto.getQuantidade());
+		Mockito.when(resultSet.getString(5)).thenReturn(produto.getLojaFornecedora());
+		
+		Mockito.when(resultSet.next()).thenReturn(false);
 	}
 	
 	@Test
 	public void cadastrar() throws Exception {
+		RepositorioProduto rp = RepositorioProduto.getInstance();
 		rp.setConexao(con);
 		rp.cadastrar("BigBig o bombom", 1.50F, 30, "Loja do Manel");
 		Mockito.verify(stmt, Mockito.times(1)).executeUpdate();
 	}
+	
 	@Test
 	public void lerProdutos() {
+		RepositorioProduto rp = RepositorioProduto.getInstance();
 		rp.setConexao(con);
-		//rp.lerProdutos();
+		rp.lerProdutos();
+		assertEquals(produto.getNome(), "TV");
 		
 	}
 	
